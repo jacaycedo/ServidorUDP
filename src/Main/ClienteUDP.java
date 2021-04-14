08bc7ecf32003b38d97195899fb34d057546a3b0
nplata1812 <53950395+nplata1812@users.noreply.github.com>
2021-04-14 10:16:04 -0500

    Cambios en clase

--- src/Main/ClienteUDP.java
+++ src/Main/ClienteUDP.java
@@ -1,5 +1,33 @@
 package Main;
 
-public class ClienteUDP {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.util.Scanner;
+
+public class ClienteUDP extends Thread{
+	private  DataOutputStream dataOutputStream = null;
+	private  DataInputStream dataInputStream = null;
+	private  int id;
+	private  int cantidadClientes;	
+	private int puerto;	
+	//private Cifrador cifrador;
+	public ClienteUDP(int id, int puerto) {
+		//cifrador=new Cifrador();
+		this.puerto = puerto;
+		this.id = id;
+		
+	}
+
+	private  void receiveFile(String fileName) throws Exception{
+		
+		
+	}
+
+
+	@Override
+	public void run() 
+	{
+		
+	}
 
 }
